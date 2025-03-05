6162ac97eb68f1d539f2a1a49865f4fb6a537773
varshitha-chowdary-2004 <varshithachowdary21@gmail.com>
2025-03-05 21:51:51 +0530

    creating separate files

---
+++ trafficlights.js
@@ -0,0 +1,15 @@
+function stop(){
+    document.getElementById("card1").style.backgroundColor='#cf1124';
+    document.getElementById("card2").style.backgroundColor='#4b5069';
+    document.getElementById("card3").style.backgroundColor='#4b5069';
+}
+function ready(){
+    document.getElementById("card1").style.backgroundColor='#4b5069';
+    document.getElementById("card2").style.backgroundColor='#f7c948';
+    document.getElementById("card3").style.backgroundColor='#4b5069';
+}
+function go(){
+    document.getElementById("card3").style.backgroundColor='#199473';
+    document.getElementById("card1").style.backgroundColor='#4b5069';
+    document.getElementById("card2").style.backgroundColor='#4b5069';
+}
